f2fe628fd9a6bd121824775aaaf2785311d57292
Fotoblysk <fotoblysk@fejm.pl>
2016-06-22 08:52:47 +0200

    Solved most memory leaks, replaced pointers handling memory with  smart_ptr (only for rarely allocated/deleted objects).

--- search/all_2.js
+++ search/all_2.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['changestate',['changeState',['../class_game.html#a82e2140cdedd171aecaed897d3aea5a0',1,'Game']]],
-  ['closed',['closed',['../class_engine_input_handler.html#ae7a6851ee3c22f14b78adc75de5cee56',1,'EngineInputHandler::closed()'],['../class_menu_input_handler.html#a493334cca9bde4229be296ad44e3db9b',1,'MenuInputHandler::closed()']]],
+  ['closed',['closed',['../class_engine_input_handler.html#a0795da79edb03ad26aa5d8aec9fc2a04',1,'EngineInputHandler::closed()'],['../class_menu_input_handler.html#a493334cca9bde4229be296ad44e3db9b',1,'MenuInputHandler::closed()']]],
   ['collisionhandler',['CollisionHandler',['../class_collision_handler.html',1,'CollisionHandler'],['../class_collision_handler.html#a1d575178fc7de4a35e9f0abb1f7f6395',1,'CollisionHandler::CollisionHandler()'],['../class_engine.html#a20fca6041ee94486e4ed1ec48d63dfef',1,'Engine::collisionHandler()']]],
   ['collisionhandler_2ecpp',['CollisionHandler.cpp',['../_collision_handler_8cpp.html',1,'']]],
   ['collisionhandler_2eh',['CollisionHandler.h',['../_collision_handler_8h.html',1,'']]],
@@ -9,6 +9,6 @@ var searchData=
   ['command_2ecpp',['Command.cpp',['../_command_8cpp.html',1,'']]],
   ['command_2eh',['Command.h',['../_command_8h.html',1,'']]],
   ['counter',['counter',['../class_enemy_spawner.html#a10072a5aab9a1d055df222ee0ef3fe6e',1,'EnemySpawner']]],
-  ['current_5fcommand_5farray',['current_command_array',['../class_input_handler.html#a6a6336cc687a47e1010b4a690dc05b40',1,'InputHandler']]],
+  ['current_5fcommand_5farray',['current_command_array',['../class_input_handler.html#abdd9f29f8c4b193f16679eddd05bdebd',1,'InputHandler']]],
   ['current_5fplayer',['current_player',['../class_engine.html#ab488c2946ac31376f272acc1eaf815b7',1,'Engine']]]
 ];
--- search/all_8.js
+++ search/all_8.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['inputhandler',['InputHandler',['../class_input_handler.html',1,'']]],
+  ['inputhandler',['InputHandler',['../class_input_handler.html',1,'InputHandler'],['../class_input_handler.html#a1b1ccf055510e8ae1fc6a4a1d1fadbe4',1,'InputHandler::InputHandler()']]],
   ['inputhandler_2ecpp',['InputHandler.cpp',['../_input_handler_8cpp.html',1,'']]],
   ['inputhandler_2eh',['InputHandler.h',['../_input_handler_8h.html',1,'']]],
   ['is_5fdead',['is_dead',['../class_ai_controler.html#aaf7042b1051e301b6b356bc6d702dccd',1,'AiControler']]],
